553ece685f84456bb99b9dd3c0f71206a77c9b56
Faisal Amir <faisal.12102223@lpu.in>
2022-03-04 21:41:29 +0530

    Modified 'welcome.blade.php' added some design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,23 @@
 <center>
-<h1>Hello world</h>
+<h1>Hello world</h1>
 
+
+<table>
+    <th><h3>REGISTRATION</h3></th>
+</table>
 
 <form action="" method="POST">
-  Username: <input type="text" name="name" /><br />
-  Email: <input type="text" name="email" /><br />
-  Password: <input type="text" name="password" /><br />
-  Confirm password: <input type="text" name="password_confirm" /><br />
+    
+  Username: <input type="text" name="name" required/><br />
+  Email: <input type="text" name="email" required/><br />
+  Password: <input type="text" name="password" required/><br />
+  Confirm password: <input type="text" name="password_confirm" required/><br />
   <input type="submit" value="Register" />
+
+
 </form>
+
+
 
 
 
