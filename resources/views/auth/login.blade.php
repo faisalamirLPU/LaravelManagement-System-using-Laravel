baa6d579fe13fb4518098a9bb20ee10b9a432900
Faisal Amir <faisal.12102223@lpu.in>
2022-03-05 20:30:00 +0530

    Added the admin panel fixed #5

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,6 +63,10 @@
                                     </a>
                                 @endif
                             </div>
+
+                            <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
                         </div>
                     </form>
                 </div>
