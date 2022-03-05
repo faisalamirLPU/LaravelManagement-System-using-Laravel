40a7fcf3edd2d291b39a55daf7207b20339f9326
Faisal Amir <faisal.12102223@lpu.in>
2022-03-05 13:03:32 +0530

    addded successfully Migration tables fixed #1

--- database/migrations/2022_03_04_110852_logs.php
+++ database/migrations/2022_03_04_110852_logs.php
@@ -21,8 +21,8 @@ class Logs extends Migration
             $table->integer('log_by')->unsigned();
             $table->foreign('log_by')->references('id')->on('users');
             //$table->string('log_by');
-            $table->string('user_type')->unsigned();
-            $table->foreign('user_type')->references('id')->on('users');
+            // $table->integer('user_type');
+            // $table->foreign('user_type')->references('id')->on('users');
             // $table->timestamp('content_writer_name');
             // $table->string('content_link');
             // $table->string('content_status');
