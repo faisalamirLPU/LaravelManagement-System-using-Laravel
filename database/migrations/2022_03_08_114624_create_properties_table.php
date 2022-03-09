5b4936be37f8b124e50201dacaecf117d3b96b2d
Faisal Amir <faisal.12102223@lpu.in>
2022-03-09 11:34:53 +0530

    added properties migtration #6

---
+++ database/migrations/2022_03_08_114624_create_properties_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->integer('id')->primary()->increments();
+            $table->string('property_name',50);
+            $table->string('property_link',300);
+            $table->string('property_slug',60);
+            $table->boolean('property_icon',60)->is_bool;
+            $table->timestamps();
+                
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
